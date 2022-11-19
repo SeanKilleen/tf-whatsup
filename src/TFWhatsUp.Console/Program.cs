ee4eea3f1b95ccd76be527cf8feeb4d07159bd97
Sean Killeen <SeanKilleen@gmail.com>
2022-11-19 00:07:00 -0500

    Squashed commit of the following:
    commit d2e477061dc0a9282d084a03acfc2b7596ef2491
    Author: Sean Killeen <SeanKilleen@gmail.com>
    Date:   Sat Nov 19 00:03:46 2022 -0500
    
        Get provider URLs
    
    commit d09db513e57f81023aab34e04c62aacd1d9e4314
    Author: Sean Killeen <SeanKilleen@gmail.com>
    Date:   Fri Nov 18 23:04:26 2022 -0500
    
        Comments
    
    commit 79d35d7c27e804da01c2db520e677581a4e44f3f
    Author: Sean Killeen <SeanKilleen@gmail.com>
    Date:   Fri Nov 18 23:02:37 2022 -0500
    
        Add HTML agility pack
    
    commit 51021fefb9d569042091bcda75a3ff3710f77451
    Author: Sean Killeen <SeanKilleen@gmail.com>
    Date:   Fri Nov 18 23:02:27 2022 -0500
    
        More
    
    commit 7387d13443de1534e3af45e7128b3f637028a72e
    Author: Sean Killeen <SeanKilleen@gmail.com>
    Date:   Fri Nov 18 22:47:02 2022 -0500
    
        More
    
    commit 94f18f3a8f88ccfbe39285ead1525462233ec1c6
    Author: Sean Killeen <SeanKilleen@gmail.com>
    Date:   Fri Nov 18 22:40:43 2022 -0500
    
        More
    
    commit 5091d723710120217fc02c50597bd8333d1a7d05
    Author: Sean Killeen <SeanKilleen@gmail.com>
    Date:   Fri Nov 18 22:31:50 2022 -0500
    
        Spit out the resources
    
    commit 0c4072ed70be938c2697a156ca591674cfa479f1
    Author: Sean Killeen <SeanKilleen@gmail.com>
    Date:   Fri Nov 18 22:13:24 2022 -0500
    
        Spectre CLI introduction
    
    commit 34383d9b4af25c0fac219e94d1bceeae2c1c285f
    Author: Sean Killeen <SeanKilleen@gmail.com>
    Date:   Fri Nov 18 21:54:37 2022 -0500
    
        Get more stuff
    
    commit e746ccfe2aa624c46df2032e7b1573cc86df0dba
    Author: Sean Killeen <SeanKilleen@gmail.com>
    Date:   Fri Nov 18 21:41:56 2022 -0500
    
        Pull version info together
    
    commit b9bed0730adb145715d5544f3f0834aa1e1506c1
    Author: Sean Killeen <SeanKilleen@gmail.com>
    Date:   Fri Nov 18 21:38:44 2022 -0500
    
        Don't need the explicit <T>
    
    commit ef54dbcb891ef08811e8a2f69d332040edf5452d
    Author: Sean Killeen <SeanKilleen@gmail.com>
    Date:   Fri Nov 18 21:37:23 2022 -0500
    
        basic parsing
    
    commit 81ffe3e4db4a874ef0a251893ac5a5b49623028b
    Author: Sean Killeen <SeanKilleen@gmail.com>
    Date:   Fri Nov 18 21:37:16 2022 -0500
    
        ignore JetBrains stuff
    
    commit 8e51b3d42df05a57bb63f9691261e0b484be2ff4
    Author: Sean Killeen <SeanKilleen@gmail.com>
    Date:   Fri Nov 18 20:57:07 2022 -0500
    
        Program and libs

---
+++ src/TFWhatsUp.Console/Program.cs
@@ -0,0 +1,123 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using HtmlAgilityPack;
+using Microsoft.Playwright;
+using Octopus.CoreParsers.Hcl;
+using Spectre.Console;
+using Spectre.Console.Cli;
+using Sprache;
+
+var app = new CommandApp<WhatsUpCommand>();
+return app.Run(args);
+internal sealed class WhatsUpCommand : AsyncCommand<WhatsUpCommand.Settings>
+{
+    public sealed class Settings : CommandSettings
+    {
+        [Description("Path where your Terraform is located. Defaults to current directory.")]
+        [CommandArgument(0, "[tfFilesPath]")]
+        public string? TerraformFilesPath { get; init; }
+    }
+
+    public override async Task<int> ExecuteAsync([NotNull] CommandContext context, [NotNull] Settings settings)
+    {
+        var StartDirectory = settings.TerraformFilesPath ?? Directory.GetCurrentDirectory();
+       
+        //TODO: Throw/exit if lockfile not found
+        //TODO: Throw/exit if no TF files found
+        var lockfileLocation = Path.Combine(StartDirectory, ".terraform.lock.hcl");
+        var allTerraformFiles = Directory.GetFiles(StartDirectory, "*.tf", SearchOption.AllDirectories);
+
+        var tfFilesTable = new Table();
+        tfFilesTable.AddColumn($"{allTerraformFiles.Length} Terraform Files Found:");
+        foreach (var tfFile in allTerraformFiles)
+        {
+            tfFilesTable.AddRow(tfFile);
+        }
+        AnsiConsole.Write(tfFilesTable);
+    
+        var parsedThing = HclParser.HclTemplate.Parse(File.ReadAllText(lockfileLocation));
+        var providers = parsedThing.Children.Where(x => x.Name == "provider").ToList();
+        var providerInfo = providers.Select(x =>
+        {
+            var valueSplit = x.Value.Split('/');
+            return new
+            {
+                providerVendor = valueSplit[1],
+                providerName = valueSplit[2],
+                urlToLatest = $"https://registry.terraform.io/providers/{valueSplit[1]}/{valueSplit[2]}/latest",
+                providerVersion = x.Children.First(x => x.Name == "version").Value
+            };
+        });
+
+        var providerTable = new Table();
+        providerTable.AddColumn("Vendor");
+        providerTable.AddColumn("Provider");
+        providerTable.AddColumn("Version");
+        providerTable.AddColumn("Registry Link");
+
+        foreach (var provider in providerInfo)
+        {
+            providerTable.AddRow(provider.providerVendor, provider.providerName, provider.providerVersion, $"[link]{provider.urlToLatest}[/]");
+        }
+        
+        AnsiConsole.Write(providerTable);
+        
+        AnsiConsole.WriteLine("Concatenating TF files");
+        var sb = new StringBuilder();
+        foreach (var tfFile in allTerraformFiles)
+        {
+            sb.Append(File.ReadAllText(tfFile));
+        }
+        
+        var parsedBigThing = HclParser.HclTemplate.Parse(sb.ToString());
+
+        var allResources = parsedBigThing.Children.Where(x => x.Name == "resource");
+        var allData = parsedBigThing.Children.Where(x => x.Name == "data");
+
+        var resourceTypes = new HashSet<string>(allResources.Select(x => x.Value));
+        var dataTypes = new HashSet<string>(allData.Select(x => x.Value));
+        var totalTypes = resourceTypes.Union(dataTypes).Order();
+
+        var table = new Table();
+        table.AddColumn($"{totalTypes.Count()} Resource Types Found:");
+        foreach (var thing in totalTypes)
+        {
+            table.AddRow(thing);
+        }
+        AnsiConsole.Write(table);
+        
+        AnsiConsole.WriteLine("Determining GitHub URLs");
+        var ghUrlTable = new Table();
+        ghUrlTable.AddColumn("Provider");
+        ghUrlTable.AddColumn("GitHub URL");
+        using (var playwright = await Playwright.CreateAsync())
+        {
+            var browser = await playwright.Chromium.LaunchAsync();
+            foreach (var provider in providerInfo)
+            {
+                AnsiConsole.WriteLine("Getting new page");
+                var page = await browser.NewPageAsync();
+                AnsiConsole.WriteLine("Navigating to URL");
+                await page.GotoAsync(provider.urlToLatest);
+                AnsiConsole.WriteLine(page.Url);
+                AnsiConsole.WriteLine("Getting link via query selector");
+                var githubLink = page.Locator(".github-source-link > a").First;
+                if (githubLink is null)
+                {
+                    AnsiConsole.MarkupLine("[red]oh noooo[/]");
+                }
+                
+                AnsiConsole.WriteLine("Getting href");
+                
+                var githubUrl = await githubLink.GetAttributeAsync("href");
+
+                AnsiConsole.WriteLine("Adding to table");
+                ghUrlTable.AddRow(provider.providerName, githubUrl);
+            }
+        }
+        
+        AnsiConsole.Write(ghUrlTable);
+        return 0;
+    }
+}
