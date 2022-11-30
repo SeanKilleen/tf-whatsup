9f8cc3e44645a935b8f0428dfa16e6624010d675
Sean Killeen <SeanKilleen@gmail.com>
2022-11-29 22:59:20 -0500

    Add a basic test to ensure everything works with CI

--- src/TFWhatsUp.Console.Tests/UnitTest1.cs
+++ src/TFWhatsUp.Console.Tests/UnitTest1.cs
@@ -1,9 +1,12 @@
+using FluentAssertions;
+
 namespace TFWhatsUp.Console.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void BasicTest_AdditionWorks()
     {
+        (1 + 1).Should().Be(2);
     }
 }
